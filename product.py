dae4ef01707e41a3bf587989bed660223ab76edd
盧思帆 <liveaskid@gmail.com>
2020-05-03 12:01:52 +0900

    add for loop

--- product.py
+++ product.py
@@ -13,4 +13,7 @@ print(products)
 
 # line 8-11 可寫成 products.append([name, price])
 
-products[0][0] # 第一個[0]為products的第0格,第二個[0]是products清單中第0格中p清單中的第0格
+# products[0][0] # 第一個[0]為products的第0格,第二個[0]是products清單中第0格中p清單中的第0格
+
+for p in products:
+	print(p[0], '的價格是', p[1])
